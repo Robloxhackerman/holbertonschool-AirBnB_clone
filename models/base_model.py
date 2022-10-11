e8fc1488ca13dd1547f1b0534a5bdc8677121151
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-10-11 06:30:52 -0700

    first commit

---
+++ models/base_model.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+TLC con china
+"""
+import datetime
+import uuid
+
+
+class BaseModel:
+    """pa despues"""
+
+    def __init__(slef, id, created_at, updated_at):
+        """pipo"""
+
+        id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        """pipo"""
+
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """pipo"""
+
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        """pipo"""
+
+        diccionaritito = self.__dict__.copy()
+        diccionaritito["__class__"] = self.__class__.__name__
+        diccionaritito["created_at"] = self.created_at.isoformat()
+        diccionaritito["updated_at"] = self.updated_at.isoformat()
