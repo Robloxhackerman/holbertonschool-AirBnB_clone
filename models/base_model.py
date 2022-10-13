c75f574e03c40d5b2bdbf4a31646dc86b56976cf
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-10-13 06:37:39 -0700

    try1

--- models/base_model.py
+++ models/base_model.py
@@ -23,6 +23,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
         """pipo"""
@@ -33,6 +34,7 @@ class BaseModel:
         """pipo"""
 
         self.updated_at = datetime.datetime.now()
+        models.storage.save(self)
 
     def to_dict(self):
         """pipo"""
---
+++ models/engine/file_storage.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""From Uganda"""
+
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    """aaaa"""
+
+    def __init__(self, file_path, objects):
+        """aaaa"""
+
+        slef.__fle_path = file_path
+        self.__objects = objects
+
+    def all(self):
+        """aaa"""
+
+        return self.__objects
+
+    def new(self, obj):
+        """aaa"""
+
+        CHICHO = obj.__class__.__name__ + "." + obj.id
+        self.__objects[CHICHO] = obj
+
+    def save(self):
+        """aaa"""
+
+        diccionarin = {}
+
+        for PEPE, PIPO in self.__objects.items():
+            diccionarin[PEPE] = PIPO.to_dict()
+        with open(self.__file_path, mode="w") as f:
+            f.write(json.dumps(diccionarin))
+
+    def reload(self):
+        """aaaaa"""
+
+        try:
+            with open(self.__file_path, mode="r") as f
+                diccionarintin = json.loads(f.read())
+                for PEPE, PIPO in diccionrintin.items():
+                    self.__object[PEPE] = eval(PIPO["__class__"])(**PIPO)
+        except Exception:
+            pass
