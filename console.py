728f99a77bc5a100d0eb36f7368d01d67dbbe6e2
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-10-14 05:59:02 -0700

    task 1o, faltaban los imports xd

--- console.py
+++ console.py
@@ -6,6 +6,11 @@ from models.base_model import BaseModel
 import models
 from models.engine.file_storage import FileStorage
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
