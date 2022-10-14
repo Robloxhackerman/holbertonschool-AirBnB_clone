4079e4d9001a31cfa07ef7240a1a22d17f632bb5
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-10-13 19:53:32 -0700

    try2 destroy

--- console.py
+++ console.py
@@ -88,3 +88,6 @@ class HBNBCommand(cmd.Cmd):
                 print("** instance id missing **")
         else:
             print("** class name missing **")
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
