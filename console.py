7f524b29359793b230c2aa8b7412993a5c55c17e
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-10-13 17:33:52 -0700

    add empty line

--- console.py
+++ console.py
@@ -2,33 +2,35 @@
 """aaaaa"""
 
 import cmd
-
+from models.base_model import BaseModel
+from models import storage
+from models.engine.file_storage import FileStorage
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
     file = None
-    clasesitas = ['BaseModel']
+    clases = ["BaseModel"]
 
     def do_quit(self, arg):
-
         return True
 
     def do_EOF(self, arg):
-
         return True
 
-    def do_create(self, arg):
-        
-        comandito = self.parseline(arg)[0]
+    def do_create(self, arg):    
+        comandito = arg.split()
 
         if comandito == None:
             print("** class name missing **")
-        elif comandito not in self.clasesitas:
+        elif comandito[0] not in self.clases:
             print("** class doesn't exist **")
         else:
-            cosita = eval(comandito)()
+            cosita = eval(comandito[0])()
             cosita.save()
             print(cosita.id)
+    
+    def emptyline(self):
+        pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
