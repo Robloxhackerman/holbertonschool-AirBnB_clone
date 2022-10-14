2f29f8773a99eeacad9057dbd36cac6619cf4a37
Robloxhackerman <zeituniannicolas27@gmail.com>
2022-10-13 17:14:42 -0700

    try222

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,15 +3,31 @@
 
 import unittest
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from models import storage
 import datetime
 
 class TestBase(unittest.TestCase):
     """aaaaa"""
-    
-    def tryaa(self):
-        """aaaaaa"""
-        basesita = BaseModel()
-        self.assertEqual(type(basesita.id), str, [])
-        self.assertEqual(type(basesita.created_at), datetime)
-        self.assertEqual(type(basesita.updated_at), datetime)
 
+    def setUp(self):
+        """aaaaa"""
+        self.basesita = BaseModel()
+    def test1(self):
+        """aaaaaa"""
+        self.assertEqual(type(self.basesita.id), str, [])
+        self.assertEqual(type(self.basesita.created_at), datetime.datetime)
+        self.assertEqual(type(self.basesita.updated_at), datetime.datetime)
+
+    def test_initt(self):
+        """tests init"""
+        self.assertTrue(isinstance(self.basesita, BaseModel))
+
+    def test_save(self):
+        """tests save"""
+        self.basesita.save()
+        self.assertNotEqual(self.basesita.created_at, self.basesita.updated_at)
+
+
+if __name__ == '__main__':
+    unittest.main()
